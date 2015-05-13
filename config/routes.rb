9ab7263e6d4e3f815c0bfe2fbe27b59ada40f6bd
Pam Willenz <pam@positek.net>
2015-05-13 17:45:01 -0400

    Assignment 27 in process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'advertisement/index'
+  get 'advertisement/show'
+  
+  resources :advertisement
+  
+
+
   resources :posts
+
 
   get 'about' => 'welcome#about'
     root to: 'welcome#index'
