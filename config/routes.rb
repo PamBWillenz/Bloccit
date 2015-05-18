32b5c7602fed47ef917496c73b33b1f065b219de
Pam Willenz <pam@positek.net>
2015-05-18 11:28:06 -0400

    made advertisement files plural

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get 'advertisement/index'
   get 'advertisement/show'
   
-  resources :advertisement
-  
+  resources :advertisements
+  # get /advertisements/index, controller: 'advertisements', action: 'index'
+  # get /advertisements/:id/show, controller: 'advertisements', action: 'show'
+  # get /advertisements/new, controller: 'advertisements', action: 'new'
+  # post /advertisements/create, controller: 'advertisements', action: 'create'
+
 
 
   resources :posts
