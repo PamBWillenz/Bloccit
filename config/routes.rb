17bf7b3e113fde5b433c93f24083fb8da704ef91
Pam Willenz <pam@positek.net>
2015-05-20 10:13:17 -0400

    Assignment 29 in process

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  # get 'question/index'
+
+  # get 'question/new'
+
+  # get 'question/create'
+
+  # get 'question/show'
+
+  # get 'question/edit'
+
+  # get 'question/update'
+
+  # get 'question/destroy'
+
+  resources :question
+
   resources :posts
 
   get 'about' => 'welcome#about'
