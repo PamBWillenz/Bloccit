63b5b756016e1257eb50bb1b58fc8977b330705d
Pam Willenz <pam@positek.net>
2015-05-20 20:52:45 -0400

    Assignment 29, working except delete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def index
-    @question = Question.all
+    @questions = Question.all
     # render 'index.html.erb'
   end
 
@@ -9,16 +9,16 @@ class QuestionsController < ApplicationController
     # render 'new.html.erb'
   end
 
-  # def create
-  #   @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
-  #   if @question.save
-  #     flash[:notice] = "Question was saved."
-  #     redirect_to @question
-  #   else
-  #     flash[:error] = "There was an error saving the question. Please try again."
-  #     render :new
-  #   end
-  # end
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
+    if @question.save
+      flash[:notice] = "Question was saved."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :new
+    end
+  end
 
   def show
     @question = Question.find(params[:id])
@@ -28,17 +28,18 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
-  # def update
-  #   @question = Question.find(params[:id])
-  #   if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
-  #     flash[:notice] = "Question was updated."
-  #     redirect_to @quesiton
-  #   else
-  #     flash[:error] = "There was an error saving the question. Please try again."
-  #     render :edit
-  #   end
-  # end
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
 
   def destroy
+    @question = Question.find(params[:id])
   end
 end
