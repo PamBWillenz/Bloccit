2966932a3316d38453eb1309cd5eb58d136e6e83
Pam Willenz <pam@positek.net>
2015-06-27 16:20:39 -0400

    Checkpoint 50 in process, mocking. Last Rspec test isn't passing

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,10 @@
  require 'rails_helper'
- 
- describe Vote do
-# pending "add some examples to (or delete) #{__FILE__}"
-# end
-   describe "validations" do
+
+describe Vote do
+
+include TestFactories
+
+  describe "validations" do
     describe "value validation" do
      it "only allows -1 or 1 as a valid value" do
         vote_up = Vote.new(value: 1)
@@ -15,4 +16,14 @@
        end
      end
    end
+ 
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
 end
