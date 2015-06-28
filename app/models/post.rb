aa964c186e9941f22a6a6d0a65680d845aac5f03
Pam Willenz <pam@positek.net>
2015-06-28 11:14:04 -0400

    Assignment 50 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -35,4 +35,9 @@ class Post < ActiveRecord::Base
   def create_vote
     user.votes.create(value: 1, post: self)
   end
+
+  def save_with_initial_vote
+    ActiveRecord::Base.transaction do
+    user.votes.create(value: 1, post: self)
+  end
 end
