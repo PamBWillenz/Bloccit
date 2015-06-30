0dbfd81a1f797e7a80b0a8ae4656e0d63cfc6eb0
Pam Willenz <pam@positek.net>
2015-06-29 21:42:39 -0400

    Checkpoint 51 in process, can't get mailer to work in rails console

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,28 @@
+class FavoritesController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: post)
+    authorize favorite
+    if favorite.save
+      flash[:notice] = "Favorite was saved."
+      redirect_to [post.topic, post]
+    else
+      flash[:error] = "There was an error saving the favorite for your post. Please try again."
+      redirect_to [post.topic,post]
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+    if favorite.destroy
+      flash[:notice] = "Removed Favorite."
+      redirect_to [post.topic, post]
+    else
+      flash[:error] = "Unable to remove Favorite. Please try again."
+      redirect_to [post.topic, post]
+    end
+  end
+end
