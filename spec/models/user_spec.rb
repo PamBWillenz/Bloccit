3fe88ab3415179874d5d04d936753fca6a38445d
Pam Willenz <pam@positek.net>
2015-07-01 14:50:40 -0400

    Assignment 51 in progress, 1 test failure

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,24 @@ describe User do
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns `nil` if the user has not favorited the post" do
+
+    before do
+      @post = associated_post
+      @user = authenticated_user(email_favorites: true)
+      @other_user = authenticated_user
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to eql(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(post: @post)
+      expect(@user.favorited(@post)).to eq(favorite)
+    end
+
+    it "returns `nil` if the user has favorited another post" do
+      expect(@user.favorited.another(@post)).to eq(nil)
     end
   end
 end
