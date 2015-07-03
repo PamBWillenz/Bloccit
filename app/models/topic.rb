779af729da9bd304c96f67b911bb4169d27f3188
Pam Willenz <pam@positek.net>
2015-07-03 08:56:18 -0400

    Assignment 52, error running rspec spec/models/topic_spec.rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,8 @@ class Topic < ActiveRecord::Base
   has_many :posts
   has_many :posts, dependent: :destroy
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  
+  scope :privately_viewable, -> {where(public: false)}
+  scope :publicly_viewable, -> {where(public: true)}
 end
