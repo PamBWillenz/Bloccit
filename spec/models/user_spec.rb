c2c6c665a19d0a06ee3d6748a28b1e5b1e1f5013
Pam Willenz <pam@positek.net>
2015-07-11 10:09:28 -0400

    Assignment 56 complete, tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 describe User do
 
-  # before do
-  #   @user = create(:user)
-  #   @post = create(:post, user: @user)
-  # end
+  include TestFactories
 
-  # describe "#favorited(post)" do
-  #   it "returns `nil` if the user has not favorited the post" do
-  #   expect ( @user.favorited(@post) ).to eq(nil)
-  # end
+  describe "#favorited(post)" do
+    xit "returns `nil` if the user has not favorited the post" do
+    end
 
-  #   it "returns the appropriate favorite if it exists" do
-  #     favorite = @user.favorites.create(post: @post)
-  #     expect ( @user.favorited(@post) ).to eq(favorite)
-  #   end
-  # end
+    xit "returns the appropriate favorite if it exists" do
+    end
+  end
 
   describe ".top_rated" do
 
